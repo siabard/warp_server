46ad8802aefb08d698ab6072fc0bcbb24f6e66c6
Yeonho Jang <siabard@gmail.com>
2023-05-10 16:35:30 +0900

    DB 등록시 금칙어 처리

--- src/routes/question.rs
+++ src/routes/question.rs
@@ -3,36 +3,12 @@ use std::collections::HashMap;
 use tracing::{event, instrument, Level};
 use warp::http::StatusCode;
 
+use crate::profanity::check_profanily;
 use crate::store::Store;
 use crate::types::pagination::extract_pagination;
 use crate::types::pagination::Pagination;
 use crate::types::question::NewQuestion;
 use crate::types::question::Question;
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct APIResponse {
-    message: String,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-struct BadWord {
-    original: String,
-    word: String,
-    deviations: i64,
-    info: i64,
-    #[serde(rename = "replacedLen")]
-    replaced_len: i64,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-struct BadWordsResponse {
-    content: String,
-    bad_words_total: i64,
-    bad_words_list: Vec<BadWord>,
-    censored_content: String,
-}
 
 #[instrument]
 pub async fn get_questions(
@@ -61,6 +37,23 @@ pub async fn update_question(
     store: Store,
     question: Question,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    let title = match check_profanily(question.title).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+
+    let content = match check_profanily(question.content).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
+
+    let question = Question {
+        id: question.id,
+        title,
+        content,
+        tags: question.tags,
+    };
+
     match store.update_question(question, id).await {
         Ok(res) => Ok(warp::reply::json(&res)),
         Err(e) => Err(warp::reject::custom(e)),
@@ -82,52 +75,18 @@ pub async fn add_question(
     store: Store,
     new_question: NewQuestion,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    let client = reqwest::Client::new();
-    let res = client
-        .post("https://api.apilayer.com/bad_words?censor_character=*")
-        .header("apikey", "PtYERikdCd9KCh5xExEybCrtBX9825vT")
-        .body("a list with shit words")
-        .send()
-        .await
-        .map_err(|e| handle_errors::Error::ExternalAPIError(e))?;
+    let title = match check_profanily(new_question.title).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
 
-    if !res.status().is_success() {
-        if res.status().is_client_error() {
-            let err = transform_error(res).await;
-            return Err(warp::reject::custom(handle_errors::Error::ClientError(err)));
-        } else {
-            let err = transform_error(res).await;
-            return Err(warp::reject::custom(handle_errors::Error::ServerError(err)));
-        }
-    }
-    /* 취소선
-        match res.error_for_status() {
-            Ok(res) => {
-                let res = res
-                    .text()
-                    .await
-                    .map_err(|e| handle_errors::Error::ExternalAPIError(e))?;
-                println!("{}", res);
-                match store.add_question(new_question).await {
-                    Ok(_) => Ok(warp::reply::with_status("Question added", StatusCode::OK)),
-                    Err(e) => Err(warp::reject::custom(e)),
-                }
-            }
-
-            Err(err) => Err(warp::reject::custom(
-                handle_errors::Error::ExternalAPIError(err),
-            )),
-    }
-         */
-
-    let res = res
-        .json::<BadWordsResponse>()
-        .await
-        .map_err(|e| handle_errors::Error::ExternalAPIError(e))?;
-    let content = res.censored_content;
+    let content = match check_profanily(new_question.content).await {
+        Ok(res) => res,
+        Err(e) => return Err(warp::reject::custom(e)),
+    };
 
     let question = NewQuestion {
-        title: new_question.title,
+        title: title,
         content,
         tags: new_question.tags,
     };
@@ -135,12 +94,5 @@ pub async fn add_question(
     match store.add_question(question).await {
         Ok(question) => Ok(warp::reply::json(&question)),
         Err(e) => Err(warp::reject::custom(e)),
-    }
-}
-
-async fn transform_error(res: reqwest::Response) -> handle_errors::APILayerError {
-    handle_errors::APILayerError {
-        status: res.status().as_u16(),
-        message: res.json::<APIResponse>().await.unwrap().message,
     }
 }
