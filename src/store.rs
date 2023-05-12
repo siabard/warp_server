f44a7ed5da23568ecb517796c9af416976e5bde5
Yeonho Jang <siabard@gmail.com>
2023-05-12 09:20:27 +0900

    `add_answer`는 데이터를 되돌려주지않도록 변경

--- src/store.rs
+++ src/store.rs
@@ -126,24 +126,30 @@ impl Store {
         }
     }
 
-    pub async fn add_answer(&self, new_answer: NewAnswer) -> Result<Answer, Error> {
-        match sqlx::query(
-	    "INSERT INTO answers (content, corresponding_question) VALUES ($1, $2) RETURNING id, content, corresponding_question"
-	).bind(new_answer.content
-	).bind(new_answer.question_id.0)
-	    .map(|row : PgRow| Answer {
-		id: AnswerId(row.get("id")),
-		content: row.get("content"),
-		question_id: QuestionId(row.get("question_id")),
-
-	    }).fetch_one(&self.connection)
-	    .await {
-		Ok(answer) => Ok(answer),
-		Err(e) => {
-		    tracing::event!(tracing::Level::ERROR, "{:?}", e);
-		    Err(Error::DatabaseQueryError(e))
-		}
-	    }
+    pub async fn add_answer(&self, new_answer: NewAnswer) -> Result<bool, Error> {
+        match sqlx::query("INSERT INTO answers (content, corresponding_question) VALUES ($1, $2)")
+            .bind(new_answer.content)
+            .bind(new_answer.question_id.0)
+            .execute(&self.connection)
+            .await
+        {
+            Ok(_) => Ok(true),
+            Err(error) => {
+                tracing::event!(
+                    tracing::Level::ERROR,
+                    code = error
+                        .as_database_error()
+                        .unwrap()
+                        .code()
+                        .unwrap()
+                        .parse::<i32>()
+                        .unwrap(),
+                    db_message = error.as_database_error().unwrap().message(),
+                    constraint = error.as_database_error().unwrap().constraint().unwrap()
+                );
+                Err(Error::DatabaseQueryError(error))
+            }
+        }
     }
 
     pub async fn add_account(&self, account: Account) -> Result<bool, Error> {
