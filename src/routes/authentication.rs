c1a196f02d7f3053071345547ea131db1386803d
Yeonho Jang <siabard@gmail.com>
2023-05-12 16:20:05 +0900

    로그인 토큰 처리

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -3,7 +3,9 @@ use rand::Rng;
 use warp::http::StatusCode;
 
 use crate::store::Store;
-use crate::types::account::Account;
+use crate::types::account::{Account, AccountId};
+
+use paseto::v2::local::local_paseto;
 
 pub async fn register(store: Store, account: Account) -> Result<impl warp::Reply, warp::Rejection> {
     let hashed_password = hash_password(account.password.as_bytes());
@@ -24,4 +26,34 @@ pub fn hash_password(password: &[u8]) -> String {
     let salt = rand::thread_rng().gen::<[u8; 32]>();
     let config = Config::default();
     argon2::hash_encoded(password, &salt, &config).unwrap()
+}
+
+pub async fn login(store: Store, login: Account) -> Result<impl warp::Reply, warp::Rejection> {
+    match store.get_account(login.email).await {
+        Ok(account) => match verify_password(&account.password, login.password.as_bytes()) {
+            Ok(verified) => {
+                if verified {
+                    Ok(warp::reply::json(&issue_token(
+                        account.id.expect("id not found"),
+                    )))
+                } else {
+                    Err(warp::reject::custom(handle_errors::Error::WrongPassword))
+                }
+            }
+            Err(e) => Err(warp::reject::custom(
+                handle_errors::Error::ArgonLibraryError(e),
+            )),
+        },
+        Err(e) => Err(warp::reject::custom(e)),
+    }
+}
+
+fn verify_password(hash: &str, password: &[u8]) -> Result<bool, argon2::Error> {
+    argon2::verify_encoded(hash, password)
+}
+
+fn issue_token(account_id: AccountId) -> String {
+    let state = serde_json::to_string(&account_id).expect("Failed to serialize");
+    local_paseto(&state, None, "RANDOM WORDS WINTER MACINTOSH PC".as_bytes())
+        .expect("Failed to create token")
 }
